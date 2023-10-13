0074068656b4a223f2aba9fe3211b4ca491e876d
Dima Danilov <122111315+dzmitreydanilov@users.noreply.github.com>
2023-10-13 13:23:09 +0200

    init

---
+++ shared/src/commonMain/kotlin/com/ddanilov/composedemo/Greeting.kt
@@ -0,0 +1,9 @@
+package com.ddanilov.composedemo
+
+class Greeting {
+    private val platform: Platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
